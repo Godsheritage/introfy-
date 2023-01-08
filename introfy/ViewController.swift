c5d2d54a7aa495565b9588ca667fefdfbde12279
Godsheritage  Adeoye <Adeoyegodsheritage@gmail.com>
2023-01-07 22:15:33 -0500

    application complete

--- introfy/ViewController.swift
+++ introfy/ViewController.swift
@@ -26,11 +26,30 @@ class ViewController: UIViewController {
     }
     
     
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBAction func introduceSelfDidTapped(_ sender: UIButton) {
+        // Lets us choose the title we have selected from the segmented control
+        // In our example that is whether it is first, second, third or forth
+        let year = yearSegmentedControl.titleForSegment(at: yearSegmentedControl.selectedSegmentIndex)
+        
+        // Creating a constant of type string that holds an introduction. The introduction receives the values from the outlet connections.
+        let introduction = """
+                    My name is \(firstNameTextField.text!) \(lastNameTextField.text!) and I attend \(schoolNameTextField.text!). I am currently in my \(year!) year and I own \(numberOfPetsLabel.text!) dogs.
+                        It is \(morePetsSwitch.isOn) that I want more pets.
+"""
+               
+        
+        
+        // Creates the alert where we pass in our message, which our introduction.
+               let alertController = UIAlertController(title: "My Introduction", message: introduction, preferredStyle: .alert)
+               
+               // A way to dismiss the box once it pops up
+               let action = UIAlertAction(title: "Nice to meet you!", style: .default, handler: nil)
+               
+               // Passing this action to the alert controller so it can be dismissed
+               alertController.addAction(action)
+               
+               present(alertController, animated: true, completion: nil)
+        
     }
     
     
