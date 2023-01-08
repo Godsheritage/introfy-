8e4e0e6a7a989c6ebc2c8613e5ce05bf8f7e9118
Godsheritage  Adeoye <Adeoyegodsheritage@gmail.com>
2023-01-07 20:50:26 -0500

    Initial Commit

---
+++ introfy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  introfy
+//
+//  Created by Godsheritage Adeoye on 1/7/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
